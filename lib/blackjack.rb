833297331a14bd9694aa2d9e042f9fabc8d8183b
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-16 01:34:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,10 +57,15 @@ end
 #####################################################
 
 def runner
-  welcome
+ welcome
   card_total = initial_round
-  hit?(card_total)
-  until
-  end_game
+  until card_total > 21
+
+    card_total = hit?(card_total)
+
+    display_card_total(card_total)
+
+  end
+  end_game(card_total)
 end
     
